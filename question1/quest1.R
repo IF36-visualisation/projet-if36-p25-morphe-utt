e7ce37cd27f39bbb01c374210a0fd8cb1fb5a68f
maximeMonterin <maxime.monterin@etu.univ-amu.fr>
2025-04-27 16:07:46 +0200

    ENFIN CA MARCHE

--- question1/quest1.R
+++ question1/quest1.R
@@ -15,7 +15,7 @@ variables_chimiques <- c("fixed_acidity", "volatile_acidity", "citric_acid",
 
 
 # 2. On factorise nos différentes variables et leurs valeurs dans un tableau à 2 dimensions.
-#ça va permettre de générer pleins de graphiques en une seule fois plutot que d'en générer plusieurs à la main
+# ça va permettre de générer pleins de graphiques en une seule fois plutot que d'en générer plusieurs à la main
 redwine2 <- redwine %>%
   select(quality, all_of(variables_chimiques)) %>%
   pivot_longer(
@@ -23,3 +23,25 @@ redwine2 <- redwine %>%
     names_to  = "variable",
     values_to = "valeur"
   )
+
+# 3. Génération des scatters plots avec le tableau factorisé B)
+ggplot(redwine2, aes(x = valeur, y = quality)) +
+  # Génération des points
+  geom_point(alpha = 0.3, shape = 1) +
+  # Génération de la droite de regression avec son intervalle de confiance
+  geom_smooth(method = "lm", se = TRUE, color = "darkred") +
+  # Sert pour la création de nos graphiques, c'est ce qui permet la répartion sur 3 colonnes 
+  # mais aussi le fait que les abcisses soient libre 
+  facet_wrap(~ variable, scales = "free_x", ncol = 3) +
+  # Utilise un thème épuré, pour faire ressortir la droite de regression je trouve ça cool
+  theme_minimal(base_size = 12) +
+  # Gestion des labels
+  labs(
+    title = "Relations entre une variable chimique d'un vin et sa note de qualité",
+    x     = "Valeur de la variable chimique",
+    y     = "Qualité (note sur 10 attribuée par un ou des expert(s))"
+  )
+
+# La droite de regression est stable quasiment de partout, on peut donc en conclure qu'il n'y a pas de lien fort
+# entre les éléments chimiques et la note du vin. Une des hypothèses qu'on pourrait tirer serait que la note 
+# pourrait être attribuée en fonction du ressentit des experts, donc par le gout, l'odeur, l'apparence etc...
