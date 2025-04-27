c833b1236fc259d83cea73c12c30a1430c11fb21
maximeMonterin <maxime.monterin@etu.univ-amu.fr>
2025-04-27 15:23:01 +0200

    Factorisation des colonnes chimiques pour factoriser la génération des graphes

--- question1/quest1.R
+++ question1/quest1.R
@@ -14,3 +14,12 @@ variables_chimiques <- c("fixed_acidity", "volatile_acidity", "citric_acid",
               "total_sulfur_dioxide", "density", "pH", "sulphates", "alcohol")
 
 
+# 2. On factorise nos différentes variables et leurs valeurs dans un tableau à 2 dimensions.
+#ça va permettre de générer pleins de graphiques en une seule fois plutot que d'en générer plusieurs à la main
+redwine2 <- redwine %>%
+  select(quality, all_of(variables_chimiques)) %>%
+  pivot_longer(
+    cols = all_of(variables_chimiques),
+    names_to  = "variable",
+    values_to = "valeur"
+  )
