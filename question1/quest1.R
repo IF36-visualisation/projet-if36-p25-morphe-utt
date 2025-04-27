ffbbe6b8310574e7e5f4a794d83bc7227060255c
maximeMonterin <maxime.monterin@etu.univ-amu.fr>
2025-04-27 16:17:24 +0200

    génération du meme graphique version vin blanc

--- question1/quest1.R
+++ question1/quest1.R
@@ -2,19 +2,26 @@ library("ggplot2")
 library("dplyr")
 library("tidyr")
 
+# ================= LA QUESTION DONNEE ================= 
+
+# Existe-t-il une corrélation forte entre certaines propriétés chimiques et la qualité du vin ?
+
+# ================= GENERATION DE GRAPHIQUES POUR REPONDRE A LA QUESTION (vin rouge) ================= 
+# Choix d'un scatter plot pour montrer une corrélation entre un élément chimique et la note d'un vin
+
 getwd()
 
 redwine <- read.csv("~/projet-if36-p25-morphe-utt/data/red_wine.csv", header = TRUE, sep = ';')
 whitewine <- read.csv("~/projet-if36-p25-morphe-utt/data/white_wine.csv", header = TRUE, sep = ';')
 
 
-# 1. On ne garde que les variables qui sont reliées à de la chimie
+# Etape 1 : On ne garde que les variables qui sont reliées à de la chimie
 variables_chimiques <- c("fixed_acidity", "volatile_acidity", "citric_acid",
               "residual_sugar", "chlorides", "free_sulfur_dioxide",
               "total_sulfur_dioxide", "density", "pH", "sulphates", "alcohol")
 
 
-# 2. On factorise nos différentes variables et leurs valeurs dans un tableau à 2 dimensions.
+# Etape 2 : On factorise nos différentes variables et leurs valeurs dans un tableau à 2 dimensions.
 # ça va permettre de générer pleins de graphiques en une seule fois plutot que d'en générer plusieurs à la main
 redwine2 <- redwine %>%
   select(quality, all_of(variables_chimiques)) %>%
@@ -24,7 +31,7 @@ redwine2 <- redwine %>%
     values_to = "valeur"
   )
 
-# 3. Génération des scatters plots avec le tableau factorisé B)
+# Etape 3 : Génération des scatters plots avec le tableau factorisé B)
 ggplot(redwine2, aes(x = valeur, y = quality)) +
   # Génération des points
   geom_point(alpha = 0.3, shape = 1) +
@@ -37,10 +44,41 @@ ggplot(redwine2, aes(x = valeur, y = quality)) +
   theme_minimal(base_size = 12) +
   # Gestion des labels
   labs(
-    title = "Relations entre une variable chimique d'un vin et sa note de qualité",
+    title = "Relations entre une variable chimique d'un vin rouge et sa note de qualité",
     x     = "Valeur de la variable chimique",
     y     = "Qualité (note sur 10 attribuée par un ou des expert(s))"
   )
+
+# ================= GENERATION DE GRAPHIQUES POUR REPONDRE A LA QUESTION (vin blanc) ================= 
+
+# Etape 2 
+whitewine2 <- whitewine %>%
+  select(quality, all_of(variables_chimiques)) %>%
+  pivot_longer(
+    cols = all_of(variables_chimiques),
+    names_to  = "variable",
+    values_to = "valeur"
+  )
+
+# Etape 3 :
+ggplot(whitewine2, aes(x = valeur, y = quality)) +
+  # Génération des points
+  geom_point(alpha = 0.3, shape = 1) +
+  # Génération de la droite de regression avec son intervalle de confiance
+  geom_smooth(method = "lm", se = TRUE, color = "darkred") +
+  # Sert pour la création de nos graphiques, c'est ce qui permet la répartion sur 3 colonnes 
+  # mais aussi le fait que les abcisses soient libre 
+  facet_wrap(~ variable, scales = "free_x", ncol = 3) +
+  # Utilise un thème épuré, pour faire ressortir la droite de regression je trouve ça cool
+  theme_minimal(base_size = 12) +
+  # Gestion des labels
+  labs(
+    title = "Relations entre une variable chimique d'un vin blanc et sa note de qualité",
+    x     = "Valeur de la variable chimique",
+    y     = "Qualité (note sur 10 attribuée par un ou des expert(s))"
+  )
+
+# ================= ANALYSE ET REPONSE A LA QUESTION DONNEE ================= 
 
 # La droite de regression est stable quasiment de partout, on peut donc en conclure qu'il n'y a pas de lien fort
 # entre les éléments chimiques et la note du vin. Une des hypothèses qu'on pourrait tirer serait que la note 
