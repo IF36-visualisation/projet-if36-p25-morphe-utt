5e5fe51936a52431ed26aa88a4d48de59eb311eb
maximeMonterin <maxime.monterin@etu.univ-amu.fr>
2025-04-27 14:17:45 +0200

    import des donnees

---
+++ question1/quest1.R
@@ -0,0 +1,5 @@
+getwd()
+
+redwine <- read.csv("~/projet-if36-p25-morphe-utt/data/red_wine.csv", header = TRUE, sep = ',')
+whitewine <- read.csv("~/projet-if36-p25-morphe-utt/data/white_wine.csv", header = TRUE, sep = ',')
+
